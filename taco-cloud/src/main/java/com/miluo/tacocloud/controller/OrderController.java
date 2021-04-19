e36536543c67c8e30d2766b55c19566b84c1d2ed
luozx <604679490@qq.com>
2021-04-19 17:08:12 +0800

    fix:修复了无法正常跳转orderForm的问题

--- taco-cloud/src/main/java/com/miluo/tacocloud/controller/OrderController.java
+++ taco-cloud/src/main/java/com/miluo/tacocloud/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.miluo.tacocloud.controller;
 
 import com.miluo.tacocloud.entity.Order;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date: 2021/4/1
  **/
 @Slf4j
-@RestController
+@Controller
 @RequestMapping("/order")
 public class OrderController {
 
